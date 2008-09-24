bb55768dd7bcd1e62da8d495c248b55db1a2a84f
Clinton Webb <webb.clint@gmail.com>
2008-09-24 17:36:40 +0800

    more protocol info

---
+++ src/p2netd/p2net.h
@@ -0,0 +1,8 @@
+// p2net.h
+#ifndef __P2NET_H
+#define __P2NET_H
+
+// global constants and other things go here.
+#define DEFAULT_PORT 14001
+
+#endif
---
+++ src/p2netd/settings.h
@@ -0,0 +1,12 @@
+// settings
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+typedef struct {
+	int port;
+	int maxconns;
+	int verbose;
+} Settings;
+
+
+#endif
